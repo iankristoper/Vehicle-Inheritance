ccadb3646b40aa6fed32bc5413b87063b57e5597
iankristoper <iankristoper26@gmail.com>
2024-03-23 09:20:55 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/vehicle_inheritance/Vehicle_Inheritance.java
@@ -0,0 +1,16 @@
+package com.ian.vehicle_inheritance;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+
+public class Vehicle_Inheritance 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
