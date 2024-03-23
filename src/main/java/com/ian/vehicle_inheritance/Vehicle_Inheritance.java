ad341e1d77aad77b3246be89c57f145fa957c4b5
iankristoper <iankristoper26@gmail.com>
2024-03-23 09:51:44 +0800

    UPDATE: main class features

--- src/main/java/com/ian/vehicle_inheritance/Vehicle_Inheritance.java
+++ src/main/java/com/ian/vehicle_inheritance/Vehicle_Inheritance.java
@@ -11,6 +11,23 @@ public class Vehicle_Inheritance
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
+        Car car = new Car("Toyota", "Camry", 2022);
+        Truck truck = new Truck("Ford", "F-2022", 2026);
+        Bicycle bike = new Bicycle("Neo", "B-1", 2026);
+        
+        System.out.println("Car: ");
+        car.accelerate();
+        car.brake();
+        car.honk();
+        
+        System.out.println("Bike: ");
+        bike.accelerate();
+        bike.brake();
+        bike.honk();
+        
+        System.out.println("Truck: ");
+        truck.accelerate();
+        truck.brake();
+        truck.honk();
     }
 }
